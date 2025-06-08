4d3121d5b894d6244131dcda89dbcb77eed33786
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-08 14:15:15 -0500

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,23 @@
+// below is class declaration, named HelloWorld
 public class HelloWorld {
+  //main method where it starts the code
   public statistic void main(String[] args) {
+    // line that will print HelloWorld to the console
     System.out.println("Hello, World!");
 
+    // line that prints my name
     System.out.println("a. My name: Hannia Escalante");
+    
+    // line that prints my favorite food
     System.out.println("b. Favorite food: ceviche");
+
+    // line that prints a celebrity I would like to meet
     System.out.println("c. Celebrity I would like to meet: Jake Gyllenhaal");
+
+    // line that prints out my least favorite chore
     System.out.println("d. Least favorite chore: Unloading dishes");
+
+    //line that prints what i would rather be doing, other than this
     System.out.println("e. What would you rather be doing, other than this: I would rather be playing with my little cousin");
   }
 }
