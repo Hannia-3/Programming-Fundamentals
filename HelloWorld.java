76e5f1b1efc26f80b2ba039e5b5bcb730255da9b
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-08 13:30:06 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,13 @@
+public class HelloWorld {
+  public statistic void main(String[] args) {
+    // Prints "Hello, World" in the terminal window
+    System.out.println("Hello, World!");
 
+    // Output personal information and preferences
+    System.out.println("a. My name: Hannia Escalante");
+    System.out.println("b. Favorite food: ceviche");
+    System.out.println("c. Celebrity I would like to meet: Jake Gyllenhaal");
+    System.out.println("d. Least favorite chore: Unloading dishes");
+    System.out.println("e. What would you rather be doing, other than this: I would rather be playing with my little cousin");
+  }
+}
