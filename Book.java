85561455eedd7b660a0984250e7838edc71d2269
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-08 20:00:23 -0500

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,32 @@
+public class Book {
+  private String title;
+  private double price;
+  private Author author;
+
+  public Book(String title, double price, Author author) {
+      this.title = title;
+      this.price = price;
+      this.author = author;
+  }
+
+  public String getTitle() {
+      return title;
+  }
+
+  public double getPrice() {
+      return price;
+  }
+
+  public Author getAuthor() {
+      return author;
+  }
+
+  @Override
+  public String toString() {
+      return "Book{" +
+             "title='" + title + '\'' + 
+             ", price=" + price + 
+             ", author= + author +
+             '}';
+  }
+}
